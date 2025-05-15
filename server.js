b1bd8fd4bb89c03ffb2a7f3a6e8386a81761398b
gboychev85 <georgi.boychev@futureplayport.com>
2025-05-15 09:22:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -57,4 +57,3 @@ app.post('/jira-comment-listener', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Listening on https://jira-comment-mention-listener.onrender.com`));
 
-app.listen(PORT, () => console.log(`🚀 Listening on port ${PORT}`));
